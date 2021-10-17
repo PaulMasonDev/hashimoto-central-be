d5701ae92d056f273af96800f76240229a3d17df
Paul Mason <webdevpaulmason@gmail.com>
2021-10-16 19:40:01 -0700

    cors issues

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 
 //TODO: GET B/E ON HEROKU, REFACTOR server.js with routes, IMPLEMENT TYPESCRIPT FOR NODE,
