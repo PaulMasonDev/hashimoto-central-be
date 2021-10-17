a3c0f0a14efb3eff9bd45e2725cf8d4c4ccc9d23
Paul Mason <webdevpaulmason@gmail.com>
2021-10-16 21:40:50 -0700

    Condensed Logic

--- server.js
+++ server.js
@@ -6,22 +6,27 @@ const app = express();
 
 app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 app.use(express.json());
 
-//TODO: GET B/E ON HEROKU, REFACTOR server.js with routes, IMPLEMENT TYPESCRIPT FOR NODE,
+//TODO: REFACTOR server.js with routes,
 //CREATE NEW ROUTES FOR DIFFERENT WEBSITES
 app.get("/getLinkList/:searchTerm", async (req, res) => {
   const { searchTerm } = req.params;
   res.send(await getHtml(`https://www.healmedelicious.com/?s=${searchTerm}`));
+});
+
+app.put("/generateCondensedRecipe", async (req, res) => {
+  const { link } = req.body;
+  res.send(await generateCondensedRecipe(link));
 });
 
 const loadHtml = async (url) => {
@@ -35,8 +40,25 @@ const loadHtml = async (url) => {
   }
 };
 
+const generateCondensedRecipe = async (url) => {
+  const $ = await loadHtml(url);
+  const ingredients = $(".tasty-recipe-ingredients li");
+  const ingredientsArr = [];
+  ingredients.each((index, value) => {
+    //console.log($(value).text());
+    ingredientsArr.push($(value).text());
+  });
+  const instructions = $(".tasty-recipe-instructions li");
+  const instructionsArr = [];
+  instructions.each((index, value) => {
+    instructionsArr.push($(value).text());
+  });
+  return { ingredientsArr, instructionsArr };
+};
+
 const getHtml = async (url) => {
   const $ = await loadHtml(url);
+  const source = "HEALMEDELICIOUS";
   const healMeDeliciousLinks = $(".entry-header");
   const linkArr = [];
   healMeDeliciousLinks.each((index, value) => {
@@ -46,10 +68,10 @@ const getHtml = async (url) => {
       .find("img")
       .attr("data-src");
     const name = $(value).find(".entry-title-link").text();
-    linkArr.push({ link, imgSrc, name });
+
+    linkArr.push({ link, imgSrc, name, source });
   });
-  const testValue = $(".entry-header a").attr("href");
-  console.log({ linkArr });
+  console.log(`FOUND ${linkArr.length} RESULTS FROM HEALMEDELICIOUS`);
   return linkArr;
 };
 
